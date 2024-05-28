dc5a9fbc55a188cc00b80dd01794b2501a68e628
Felix Gratzkowski <FeGra99@web.de>
2024-05-28 18:47:07 +0200

    feature:better roulette

--- image_roulette copy.js
+++ image_roulette copy.js
@@ -1,39 +1,42 @@
 (function() {
     console.log("image_roulette.js import"); // Konsolenausgabe hinzufügen
     // Ihr JavaScript-Code hier
+   
+   
+    function roulette(images,index){
+
+
+    
+        images[index.value].classList.add("show-1");
+        images[index.value].classList.remove('show-4', 'show-3','show-2');
+        index.value = (index.value + 1) % images.length;
+
+        images[index.value].classList.add("show-2");
+        images[index.value].classList.remove('show-1','show-4','show-3');
+        index.value = (index.value + 1) % images.length;
+
+        images[index.value].classList.add("show-3");
+        images[index.value].classList.remove('show-2','show-1','show-4');
+        index.value = (index.value + 1) % images.length;
+
+        images[index.value].classList.add("show-4");
+        images[index.value].classList.remove('show-3','show-2','show-1');
+
+    }
+
+
     document.addEventListener('DOMContentLoaded', function() {
        
         window.addEventListener('load', function() {
-            var images = document.querySelectorAll('.roulette-images img');
-            console.log(images);
-            var index = 0;
-            
-            var interval = setInterval(function () {
+            var images = this.document.querySelectorAll('.roulette-images img');
 
-                
-                images[index].classList.add("show-1");
-                images[index].classList.remove('show-4', 'show-3','show-2');
-                index = (index + 1) % images.length;
-
-                images[index].classList.add("show-2");
-                images[index].classList.remove('show-1','show-4','show-3');
-                index = (index + 1) % images.length;
-
-                images[index].classList.add("show-3");
-                images[index].classList.remove('show-2','show-1','show-4');
-                index = (index + 1) % images.length;
-
-                images[index].classList.add("show-4");
-                images[index].classList.remove('show-3','show-2','show-1');
-
-               
-
-                
-
-                
-                
-                
-            }, 5000); // Ändere die Zeit in Millisekunden nach Bedarf (hier 3000ms = 3 Sekunden)
+            [...this.document.getElementsByClassName("start")].forEach(element => {
+                element.classList = [];
+            });
+           
+            var index = {value:0};
+            roulette(images,index)
+            setInterval(() => roulette(images,index), 5000); // Ändere die Zeit in Millisekunden nach Bedarf (hier 3000ms = 3 Sekunden)
         });
     });
 })();
